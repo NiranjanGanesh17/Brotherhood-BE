7f91d2ce357296e1e2e49dd15a9f89888b416767
ct133-niranjan-g <ct133-niranjan-g@ct133-niranjan-gs-MacBook-Pro.local>
2024-08-14 19:32:34 +0530

    added avatart cahnges

--- models/users.model.js
+++ models/users.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     userName:{type: String},
-    avatar:{type:String,required:true},
+    avatar:{type:String,required:false,default:''},
     visibility:{type:Boolean,default:true},
     globalName:{type:String,required:true},
     email:{type:String,required:true,unique:true},
